4d2a0bd75dcc987d84f2a07d1f597d6a21bf14de
Kiong <kiong90@gmail.com>
2015-06-14 23:05:31 +0800

    added a sum function

---
+++ Chap 4 - Objects & Arrays/The Sum Of A Range.js
@@ -0,0 +1,19 @@
+var range = function (start, end) {
+	var arrayOfNumbers = [];
+	for(var i = start;i<=end;i++){
+		arrayOfNumbers.push(i);
+	}
+	return arrayOfNumbers;
+}
+
+console.log(range(1,10));
+
+var sum = function (arrayOfNumbers) {
+	var value = 0;
+	for(var i = 0, j=arrayOfNumbers.length;i<j;i++){
+		value += arrayOfNumbers[i];
+	}
+	return value;
+}
+
+console.log(sum(range(1,10)));
