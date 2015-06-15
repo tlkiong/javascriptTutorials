ffd1746e3d152a9199a8703d62119818464c52ba
Kiong <kiong90@gmail.com>
2015-06-16 00:18:24 +0800

    done listToArray function

--- Chap 4 - Objects & Arrays/A List.js
+++ Chap 4 - Objects & Arrays/A List.js
@@ -1,15 +1,17 @@
+var arrays = [1,2,3];
+
 var arrayToList = function(array) {
 	var arrayLength = array.length;
 	var object = {};
 	for(var i = arrayLength-1; i>=0;i--){
 		if(i == arrayLength-1) {
 			object = {
-				value: array[i],
+				val: array[i],
 				rest: null
 			}
 		} else if (i<arrayLength){
 			object = {
-				value:array[i],
+				val:array[i],
 				rest:object
 			}
 		}
@@ -18,4 +20,20 @@ var arrayToList = function(array) {
 	return object;
 }
 
-console.log(arrayToList([1,2,3]));
+console.log(arrayToList(arrays));
+
+var listToArray = function(list){
+	var array = [];
+	for(var key in list){
+		if(list.hasOwnProperty(key)){
+			//key + list[key]
+			array.push(list[key].val);
+			while(list[key].rest!=null){// will be infinite loop since never go to the inside one
+				array.push(list[key].val);
+			}
+		}
+	}
+	return array;
+}
+
+console.log(listToArray(arrayToList(arrays)));
